2147c5ec77f4ad904f35b7e04d4f5283761aa0d9
i-dont-know238 <idontknowpython123@gmail.com>
2025-10-07 22:13:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def proxy(path=''):
         return Response(f"Error: {str(e)}", status=502)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
